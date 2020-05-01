d3549da0d33d74a2cc01a03debbc3dec6b789660
matheus Calaça <matheusfcalaca@gmail.com>
2020-05-01 02:14:34 -0300

    alterado o nome do perfil

--- src/main/java/br/com/natheuscalaca/timemaluco/perfil/model/Perfil.java
+++ src/main/java/br/com/natheuscalaca/timemaluco/perfil/model/Perfil.java
@@ -1,5 +1,56 @@
 package br.com.natheuscalaca.timemaluco.perfil.model;
 
+import javax.persistence.*;
+import java.util.Objects;
 
+@Entity
+@Table(name = "PERFIL")
 public class Perfil {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "perfil_nome")
+    private Integer id;
+    @Column(name = "prefil_nome")
+    private String nome;
+    @Column(name = "perfil_cor")
+    private String cor;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Perfil perfil = (Perfil) o;
+        return Objects.equals(id, perfil.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
 }
