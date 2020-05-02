41703a2b113d0f3db296c58365e5171df5489d51
matheus Calaça <matheusfcalaca@gmail.com>
2020-05-01 21:53:44 -0300

    alterado control de usuario para pessoa

--- src/main/java/br/com/natheuscalaca/timemaluco/PessoaControl.java
+++ src/main/java/br/com/natheuscalaca/timemaluco/PessoaControl.java
@@ -4,10 +4,10 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("/api/usuario")
+@Path("/api/pessoa")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
-public class UsuarioControl {
+public class PessoaControl {
 
 
 
