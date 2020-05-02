6c6b1784f8a3a2881756fd2cfc84d125df9861bc
matheus Calaça <matheusfcalaca@gmail.com>
2020-05-01 21:25:04 -0300

    criado o control do cadastro

--- src/main/java/br/com/natheuscalaca/timemaluco/PerfilControl.java
+++ src/main/java/br/com/natheuscalaca/timemaluco/PerfilControl.java
@@ -12,7 +12,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
-@Path("/perfil")
+@Path("/api/perfil")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class PerfilControl {
